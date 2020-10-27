31eb142dfba18566d390a69d93d23c378b219a16
KGESH <baram789456@gmail.com>
2020-10-27 20:15:13 +0900

    Date Module

---
+++ date.h
@@ -0,0 +1,16 @@
+#ifndef DATE_H
+#define DATE_H
+
+#include <QObject>
+
+class Date : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Date(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // DATE_H
